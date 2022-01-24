814a3aed125bcbe1065d053c8a81ab45e4e69ca8
dhanashreejadhav2902 <dhanashreejadhav@benchmarkit.solutions>
2022-01-24 13:40:42 +0530

    Thursday To Saturday Assignment

---
+++ DemoProductProjectNew/DemoProductProjectNew/Controllers/ProductsController.cs
@@ -0,0 +1,59 @@
+﻿using DemoProductProjectNew.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoProductProjectNew.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Product> p = new List<Product>()
+            {
+                new Product(){ pid=1,pname="Laptop",price=30000},
+                new Product(){ pid=2,pname="TV",price=50000},
+                new Product(){ pid=3,pname="Mobile",price=10000},
+                new Product(){ pid=4,pname="Headphones",price=2000}
+
+            };
+            ViewBag.p = p;
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            List<Product> p = new List<Product>()
+            {
+                new Product(){ pid=1,pname="Laptop",price=30000},
+                new Product(){ pid=2,pname="TV",price=50000},
+                new Product(){ pid=3,pname="Mobile",price=10000},
+                new Product(){ pid=4,pname="Headphones",price=2000}
+
+            };
+
+            Product matchingProduct = null;
+            foreach (var item in p)
+            {
+                if (item.pid == id)
+                {
+                    matchingProduct = item;
+                }
+            }
+
+            ViewBag.MatchingProduct = matchingProduct;
+            return View();
+        }
+
+        public ActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product p) {
+            return View();
+        }
+    }
+}
---
+++ DemoProductProjectNew/DemoProductProjectNew/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoProductProjectNew.Models
+{
+    public class Product
+    {
+
+        public int pid { get; set; }
+        public string pname { get; set; }
+
+        public int price { get; set; }
+    }
+}
