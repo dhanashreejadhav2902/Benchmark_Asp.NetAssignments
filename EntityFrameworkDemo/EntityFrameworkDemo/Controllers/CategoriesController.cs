3eeec1e42ff6cefc9ebc121c48580bb40b947913
dhanashreejadhav2902 <dhanashreejadhav@benchmarkit.solutions>
2022-01-25 23:45:59 +0530

    CRUD Assignment From Mon To Thursday

---
+++ EntityFrameworkDemo/EntityFrameworkDemo/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using EntityFrameworkDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameworkDemo.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            ProductDbEntities1 db = new ProductDbEntities1();
+            List<Category> catogires=db.Categories.ToList();
+            return View(catogires);
+        }
+    }
+}
