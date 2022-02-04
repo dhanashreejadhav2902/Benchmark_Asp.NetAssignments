054d80d87a4a64de1f4e16a163ed43707b3c36db
dhanashreejadhav2902 <dhanashreejadhav@benchmarkit.solutions>
2022-02-04 17:30:00 +0530

    Udemy Demo Project

---
+++ ClinicAppNew/ClinicAppNew/Controllers/DoctorController.cs
@@ -0,0 +1,154 @@
+﻿using ClinicAppNew.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClinicAppNew.Controllers
+{
+        public class DoctorController : Controller
+        {
+
+             ClinicAppEntities db = new ClinicAppEntities();
+           public ActionResult Index(string search = "", string sortcolm = "dname", string icon = "fa-sort-desc", int pageno = 1)
+            {
+            
+                //display data without any condition
+                //List<Doctor> doctors=db.Doctors.ToList();
+
+
+            //to display data on perticular condition
+            List<Doctor> doctors = db.Doctors.Where(temp => temp.dname.Contains(search)).ToList();
+                ViewBag.Search = search;
+
+
+
+                ViewBag.sortcolm = sortcolm;
+                ViewBag.icon = icon;
+                if (ViewBag.sortcolm == "did")
+                {
+                    if (ViewBag.icon == "fa-sort-asc")
+                        doctors = doctors.OrderBy(temp => temp.did).ToList();
+                    else
+                        doctors = doctors.OrderByDescending(temp => temp.did).ToList();
+
+                }
+                else if (ViewBag.sortcolm == "dname")
+                {
+
+                    if (ViewBag.icon == "fa-sort-asc")
+                        doctors = doctors.OrderBy(temp => temp.dname).ToList();
+                    else
+                        doctors = doctors.OrderByDescending(temp => temp.dname).ToList();
+
+                }
+                else if (ViewBag.sortcolm == "specialist")
+                {
+
+                    if (ViewBag.icon == "fa-sort-asc")
+                        doctors = doctors.OrderBy(temp => temp.specialist).ToList();
+                    else
+                        doctors = doctors.OrderByDescending(temp => temp.specialist).ToList();
+
+
+                }
+                else if (ViewBag.sortcolm == "mobile")
+                {
+                    if (ViewBag.icon == "fa-sort-asc")
+                        doctors = doctors.OrderBy(temp => temp.mobile).ToList();
+                    else
+                        doctors = doctors.OrderByDescending(temp => temp.mobile).ToList();
+
+
+                }
+                else if (ViewBag.sortcolm == "email")
+                {
+                    if (ViewBag.icon == "fa-sort-asc")
+                        doctors = doctors.OrderBy(temp => temp.email).ToList();
+                    else
+                        doctors = doctors.OrderByDescending(temp => temp.email).ToList();
+
+                }
+                else if (ViewBag.sortcolm == "address")
+                {
+                    if (ViewBag.icon == "fa-sort-asc")
+                        doctors = doctors.OrderBy(temp => temp.address).ToList();
+                    else
+                        doctors = doctors.OrderByDescending(temp => temp.address).ToList();
+
+                }
+                else if (ViewBag.sortcolm == "description")
+                {
+                    if (ViewBag.icon == "fa-sort-asc")
+                        doctors = doctors.OrderBy(temp => temp.description).ToList();
+                    else
+                        doctors = doctors.OrderByDescending(temp => temp.description).ToList();
+
+                }
+
+                int recordperpage = 5;
+                int totalpage = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(doctors.Count) / Convert.ToDouble(recordperpage)));
+                int pageredcordtoskip = (pageno - 1) * recordperpage;
+                ViewBag.pageno = pageno;
+                ViewBag.totalpage = totalpage;
+                doctors = doctors.Skip(pageredcordtoskip).Take(recordperpage).ToList();
+                return View(doctors);
+            }
+
+            public ActionResult Details(int id)
+            {
+               
+                Doctor d = db.Doctors.Where(temp => temp.did == id).FirstOrDefault();
+                return View(d);
+            }
+
+            //this is get method to get create request from user
+            public ActionResult Create()
+            {
+                
+                return View();
+            }
+
+            //this is post request to insert data from user in form
+            [HttpPost]
+            public ActionResult Create(Doctor d)
+            {
+                db.Doctors.Add(d);
+                db.SaveChanges();
+                return RedirectToAction("index");
+            }
+
+
+            public ActionResult Edit(int id)
+            {
+                Doctor d = db.Doctors.Where(temp => temp.did == id).FirstOrDefault();
+                return View(d);
+            }
+
+            [HttpPost]
+            public ActionResult Edit(Doctor d)
+            {
+               
+                Doctor oldDoctor = db.Doctors.Where(temp => temp.did == d.did).FirstOrDefault();
+                oldDoctor.dname = d.dname;
+                oldDoctor.email = d.email;
+                oldDoctor.specialist = d.specialist;
+                oldDoctor.mobile = d.mobile;
+                oldDoctor.address = d.address;
+                oldDoctor.description = d.description;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+        public ActionResult Delete(int id)
+        {
+
+            Doctor d = db.Doctors.Where(temp => temp.did == id).FirstOrDefault();
+            db.Doctors.Remove(d);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+       }
+    }
---
+++ ClinicAppNew/ClinicAppNew/Controllers/PatientController.cs
@@ -0,0 +1,164 @@
+﻿using ClinicAppNew.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClinicAppNew.Controllers
+{
+    public class PatientController : Controller
+    {
+        ClinicAppEntities db = new ClinicAppEntities();
+        public ActionResult Index(string search = "", string sortcolm = "pname", string icon = "fa-sort-desc", int pageno = 1)
+        {
+            
+            //display data without any condition
+            //List<Product> products=db.Products.ToList();
+
+
+            //to display data on perticular condition
+            List<Patient> patient = db.Patients.Where(temp => temp.pname.Contains(search)).ToList();
+            ViewBag.Search = search;
+            ViewBag.sortcolm = sortcolm;
+            ViewBag.icon = icon;
+            if (ViewBag.sortcolm == "pid")
+            {
+                if (ViewBag.icon == "fa-sort-asc")
+                    patient = patient.OrderBy(temp => temp.pid).ToList();
+                else
+                    patient = patient.OrderByDescending(temp => temp.pid).ToList();
+
+            }
+            else if (ViewBag.sortcolm == "pname")
+            {
+
+                if (ViewBag.icon == "fa-sort-asc")
+                    patient = patient.OrderBy(temp => temp.pname).ToList();
+                else
+                    patient = patient.OrderByDescending(temp => temp.pname).ToList();
+
+            }
+            else if (ViewBag.sortcolm == "mobile")
+            {
+                if (ViewBag.icon == "fa-sort-asc")
+                    patient = patient.OrderBy(temp => temp.mobile).ToList();
+                else
+                    patient = patient.OrderByDescending(temp => temp.mobile).ToList();
+
+
+            }
+            else if (ViewBag.sortcolm == "email")
+            {
+                if (ViewBag.icon == "fa-sort-asc")
+                    patient = patient.OrderBy(temp => temp.email).ToList();
+                else
+                    patient = patient.OrderByDescending(temp => temp.email).ToList();
+
+            }
+            else if (ViewBag.sortcolm == "paddress")
+            {
+                if (ViewBag.icon == "fa-sort-asc")
+                    patient = patient.OrderBy(temp => temp.paddress).ToList();
+                else
+                    patient = patient.OrderByDescending(temp => temp.paddress).ToList();
+
+            }
+            else if (ViewBag.sortcolm == "description")
+            {
+                if (ViewBag.icon == "fa-sort-asc")
+                    patient = patient.OrderBy(temp => temp.description).ToList();
+                else
+                    patient = patient.OrderByDescending(temp => temp.description).ToList();
+
+            }
+            else if (ViewBag.sortcolm == "healthinsurance")
+            {
+                if (ViewBag.icon == "fa-sort-asc")
+                    patient = patient.OrderBy(temp => temp.healthinsurance).ToList();
+                else
+                    patient = patient.OrderByDescending(temp => temp.healthinsurance).ToList();
+
+            }
+
+            else if (ViewBag.sortcolm == "did")
+            {
+                if (ViewBag.icon == "fa-sort-asc")
+                    patient = patient.OrderBy(temp => temp.Doctor.dname).ToList();
+                else
+                    patient = patient.OrderByDescending(temp => temp.Doctor.dname).ToList();
+
+            }
+
+
+            int recordperpage = 5;
+            int totalpage = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(patient.Count) / Convert.ToDouble(recordperpage)));
+            int pageredcordtoskip = (pageno - 1) * recordperpage;
+            ViewBag.pageno = pageno;
+            ViewBag.totalpage = totalpage;
+            patient = patient.Skip(pageredcordtoskip).Take(recordperpage).ToList();
+            return View(patient);
+        }
+
+        public ActionResult Details(int id)
+        {
+            
+            Patient p = db.Patients.Where(temp => temp.pid == id).FirstOrDefault();
+            return View(p);
+        }
+
+        //this is get method to get create request from user
+        public ActionResult Create()
+        {
+            
+            ViewBag.Doctor = db.Doctors.ToList();
+            return View();
+        }
+
+        //this is post request to insert data from user in form
+        [HttpPost]
+        public ActionResult Create(Patient p)
+        {
+           
+            db.Patients.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            Patient p = db.Patients.Where(temp => temp.pid == id).FirstOrDefault();
+            ViewBag.Doctor = db.Doctors.ToList();
+            return View(p);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Patient p)
+        {
+            
+            Patient oldProduct = db.Patients.Where(temp => temp.pid == p.pid).FirstOrDefault();
+            oldProduct.pname = p.pname;
+            oldProduct.mobile = p.mobile;
+            oldProduct.email = p.email;
+            oldProduct.paddress = p.paddress;
+            oldProduct.description = p.description;
+            oldProduct.healthinsurance = p.healthinsurance;
+            oldProduct.did = p.did;
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            
+            Patient p = db.Patients.Where(temp => temp.pid == id).FirstOrDefault();
+            db.Patients.Remove(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+       
+    }
+}
